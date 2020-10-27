3127c6e10e00ed244c8ec3fbd506984ed730e22a
Noel Delgado <auhcsei@gmail.com>
2020-10-27 16:21:36 -0700

    chore(style) update naming for consistency

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,17 +4,17 @@ import { Snackbar } from '@material-ui/core';
 import useStore from '../store';
 
 export default function Banner() {
-  const [globalState, globalActions] = useStore();
+  const [globalStore, globalActions] = useStore();
 
   return (
     <Snackbar
-      key={globalState.bannerMessage}
+      key={globalStore.bannerMessage}
       anchorOrigin={{
         vertical: 'bottom',
         horizontal: 'left',
       }}
-      open={globalState.banner}
-      message={globalState.bannerMessage}
+      open={globalStore.banner}
+      message={globalStore.bannerMessage}
       autoHideDuration={6000}
       onClose={() => globalActions.app.setBanner(false)}
     />
