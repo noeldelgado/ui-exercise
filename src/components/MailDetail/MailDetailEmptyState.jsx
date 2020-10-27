5678aad51c27a4d5976e9f1201e6c77b1b6042e3
Noel Delgado <auhcsei@gmail.com>
2020-10-27 15:54:11 -0700

    chore(MailDetailEmptyState) custom colors for fake storage feedback

--- src/components/MailDetail/MailDetailEmptyState.jsx
+++ src/components/MailDetail/MailDetailEmptyState.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { Box, LinearProgress, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: theme.spacing(1),
+    backgroundColor: theme.palette.grey[300],
+  },
+  bar: {
+    backgroundColor: theme.palette.grey[500],
+  },
+}));
 
 export default function MailDetailEmptyState() {
+  const classes = useStyles();
+
   return (
     <Box
       display="flex"
@@ -17,7 +30,7 @@ export default function MailDetailEmptyState() {
             You are currently using 4.73 GB (31 %) of your 15 GB.
           </Typography>
         </Box>
-        <LinearProgress variant="determinate" value={31} color="secondary" />
+        <LinearProgress variant="determinate" value={31} classes={classes} />
       </Box>
     </Box>
   );
