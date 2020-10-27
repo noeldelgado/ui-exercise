7f1ec92bb008ede7d30d7e81d5cc938d3321a978
Noel Delgado <auhcsei@gmail.com>
2020-10-26 18:31:00 -0700

    chore(layout) add basic layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Box, Fade } from '@material-ui/core';
+
 import LoadingScreen from './components/LoadingScreen';
+import Nav from './components/Nav';
 
 export default function App() {
   return (
-    <LoadingScreen/>
+    <>
+      <Nav />
+      <Box component="main">
+        Main
+      </Box>
+      <Fade
+        unmountOnExit
+        in={false}
+        timeout={{ enter: 0, exit: 200 }}
+      >
+        <Box>
+          <LoadingScreen />
+        </Box>
+      </Fade>
+    </>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+
+export default function Nav() {
+  return (
+    <Box
+      component="aside"
+      border={1}
+      borderTop={0}
+      borderBottom={0}
+      borderLeft={0}
+      borderColor="divider"
+    >
+      Nav
+    </Box>
+  );
+}
