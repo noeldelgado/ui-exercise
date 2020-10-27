ab0ddc92c6da1c3606416714e91e67283924259a
Noel Delgado <auhcsei@gmail.com>
2020-10-27 13:26:46 -0700

    feat(theme) provide basic custom theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { blue, red } from '@material-ui/core/colors';
 
 import App from './App.jsx';
 import './index.css';
 
+const theme = {
+  palette: {
+    text: {
+      primary: '#202128',
+      secondary: '#5f6368',
+    },
+    action: {
+      selected: '#f5f5f5',
+    },
+    background: {
+      default: '#fff',
+      paper: '#fff',
+    },
+    divider: '#e6e6e6',
+    primary: {
+      main: red[500],
+    },
+    secondary: {
+      main: blue[500],
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '*': {
+          'scrollbar-width': 'thin',
+        },
+        '*::-webkit-scrollbar': {
+          width: '4px',
+          height: '4px',
+        },
+        '*::-webkit-scrollbar-thumb': {
+          backgroundColor: '#e6e6e6',
+        },
+      },
+    },
+  },
+  props: {
+    MuiButtonBase: {
+      disableRipple: true,
+    },
+  },
+};
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={createMuiTheme(theme)}>
     <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root'),
 );
 
