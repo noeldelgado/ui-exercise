4e0acefc0bc8fa7e57d17e884e2bb162a21b1911
Noel Delgado <auhcsei@gmail.com>
2020-10-27 14:19:59 -0700

    fix(Nav) logo positioning mobile

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box, Button, Divider, Hidden } from '@material-ui/core';
 import { AddRounded } from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles';
 
 import NavUserInfo from './NavUserInfo';
 import NavListItems from './NavListItems';
@@ -9,17 +8,8 @@ import NavListTags from './NavListTags';
 import useStore from '../../store';
 import logo from '../../logo.png';
 
-const useStyles = makeStyles((theme) => ({
-  overflow: {
-    overflow: 'auto',
-    height: '100%',
-    paddingBottom: theme.spacing(2),
-  },
-}));
-
 export default function Nav() {
   const [globalStore, globalActions] = useStore();
-  const classes = useStyles();
 
   return (
     <Box
@@ -30,30 +20,31 @@ export default function Nav() {
       borderLeft={0}
       borderColor="divider"
     >
-      <Box px={2} height={46} display="flex" alignItems="center">
+      <Box px={2} minHeight={46}>
+        <Hidden smUp>
+          <Box p={2} />
+        </Hidden>
         <img src={logo} alt="logo" />
       </Box>
       <Divider />
-      <Box className={classes.overflow}>
-        <Hidden xsDown>
-          <NavUserInfo model={globalStore.user} />
-          <Box py={2} px={2}>
-            <Button
-              disableElevation
-              fullWidth
-              variant="outlined"
-              color="primary"
-              startIcon={<AddRounded />}
-              onClick={() => globalActions.app.setComposeEmailOpen(true)}
-            >
-              Compose
-            </Button>
-          </Box>
-        </Hidden>
-        <NavListItems />
-        <NavListTags />
-        <Box px={2}>&nbsp;</Box>
-      </Box>
+      <Hidden xsDown>
+        <NavUserInfo model={globalStore.user} />
+        <Box py={2} px={2}>
+          <Button
+            disableElevation
+            fullWidth
+            variant="outlined"
+            color="primary"
+            startIcon={<AddRounded />}
+            onClick={() => globalActions.app.setComposeEmailOpen(true)}
+          >
+            Compose
+          </Button>
+        </Box>
+      </Hidden>
+      <NavListItems />
+      <NavListTags />
+      <Box p={1}>&nbsp;</Box>
     </Box>
   );
 }
